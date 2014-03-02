12390339677d0fd9bae41adf523df0322743c378
Martin W <martas@imm.cz>
2014-03-02 12:48:36 +0100

    Update controller_skin.py

--- controller_skin.py
+++ controller_skin.py
@@ -1,17 +1,11 @@
+from PyQt5.QtCore import *
+from PyQt5.QtGui  import *
+from PyQt5.QtWidgets import *
 
-try:
-    from PyQt5.QtCore import *
-    from PyQt5.QtGui  import *
-    from PyQt5.QtWidgets import *
+Signal = pyqtSignal
+Slot = pyqtSlot
+Property = pyqtProperty 
 
-    Signal = pyqtSignal
-    Slot = pyqtSlot
-    Property = pyqtProperty 
-    
-except:
-    from PySide.QtCore import *
-    from PySide.QtGui  import *
- 
 
 INVERT_COLORS = True
 
